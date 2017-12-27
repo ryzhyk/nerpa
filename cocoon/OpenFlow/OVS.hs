c528dbc7187b087cfb142e69ba19e3fd8cf76b84
Leonid Ryzhyk <lryzhyk@vmware.com>
2017-12-26 16:21:26 -0800

    backend for new OVS builtins

--- cocoon/OpenFlow/OVS.hs
+++ cocoon/OpenFlow/OVS.hs
@@ -205,6 +205,10 @@ mkAction _ (OF.ActionBuiltin "ct_lb" [zone,OF.EVal (OF.Value _ 0),OF.EVal (OF.Va
 mkAction _ (OF.ActionBuiltin "ct_lb" [zone,OF.EVal (OF.Value _ 1),_,OF.EVal (OF.Value _ ip6)])     = "ct(commit, zone=" <> mkExprA zone <> ", nat(dst=" <> mkVal IP6 ip6 <> "))"
 mkAction _ (OF.ActionBuiltin "ct_lb" [zone,OF.EVal (OF.Value _ 0),OF.EVal (OF.Value _ ip4),_,port])= "ct(commit, zone=" <> mkExprA zone <> ", nat(dst=" <> mkVal IP4 ip4 <> ":" <> mkExprA port <> "))"
 mkAction _ (OF.ActionBuiltin "ct_lb" [zone,OF.EVal (OF.Value _ 1),_,OF.EVal (OF.Value _ ip6),port])= "ct(commit, zone=" <> mkExprA zone <> ", nat(dst=" <> mkVal IP6 ip6 <> ":" <> mkExprA port <> "))"
+mkAction _ (OF.ActionBuiltin "ct_snat" [zone,OF.EVal (OF.Value _ ip4)]) = "ct(commit, zone=" <> mkExprA zone <> ", nat(src=" <> mkVal IP4 ip4 <> "))"
+mkAction _ (OF.ActionBuiltin "ct_snat" [zone])           = "ct(commit, zone=" <> mkExprA zone <> ", nat)"
+mkAction _ (OF.ActionBuiltin "ct_dnat" [zone,OF.EVal (OF.Value _ ip4)]) = "ct(commit, zone=" <> mkExprA zone <> ", nat(dst=" <> mkVal IP4 ip4 <> "))"
+mkAction _ (OF.ActionBuiltin "ct_dnat" [zone])           = "ct(commit, zone=" <> mkExprA zone <> ", nat)"
 mkAction _ (OF.ActionBuiltin f as)                       = error $ "OVS.mkAction: unknown action " ++ f ++ " " ++ show as
     --"controller(userdata=" <> (hcat $ punctuate "." $ map (pp . (\w -> (printf "%02x" w) :: String)) u) <> ")"
 
